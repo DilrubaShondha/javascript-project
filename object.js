085a335fd2bb7fb1a05006350692535592e989cb
DilrubaShondha <dilrubaeasmin2867@gmail.com>
2024-09-14 23:13:30 +0600

    object commit

---
+++ object.js
@@ -0,0 +1,19 @@
+const student = {
+    name : 'Shakib Khan',
+    id : 111,
+    address : 'talipara',
+    isSingle : true,
+    friends:['Apu','Bubly'],
+    act: function(){
+        console.log('hihihi')
+    },
+        car:{
+        brand: 'BMW',
+        watch : 'rolex',
+        price: 200000,
+        manufacturer:{
+            CEO: 'Sundar Pandey'
+        }
+    }
+}
+student.act();
