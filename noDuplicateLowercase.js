fc74f0ccd1f123471e8463fad66a36a7f6763677
DilrubaShondha <dilrubaeasmin2867@gmail.com>
2024-09-19 20:41:38 +0600

    dom commit

---
+++ noDuplicateLowercase.js
@@ -0,0 +1,14 @@
+// array has some duplicate elements
+const names = ['Sajol', 'Shondha', 'Sajol', 'Munia', 'Munia', 'Mukit', 'Raisa', 'Sabit', 'Raisa'];
+
+function noDuplicate(array){
+    const unique =[];
+for(const name of array){
+    if(unique.includes(name) === false){
+        uniquetoLowerCase.push;
+    }
+}
+return unique;
+}
+const uniqueName = noDuplicate(names);
+console.log(uniqueName);
