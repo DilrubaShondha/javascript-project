490aaa20f9adbc646f56b1423f49472a929d83f7
DilrubaShondha <dilrubaeasmin2867@gmail.com>
2024-09-27 14:18:35 +0600

    arrow map  commit

---
+++ arrow map.js
@@ -0,0 +1,3 @@
+const num = [12,21,43,12,32];
+const double =num.map(num => num * 2);
+console.log(double);
--- destrucure.js
+++
@@ -1,9 +0,0 @@
-const actor ={
- name: 'Ananta',
- age: 30,
- phone: 01789273654,
-}  
-const {name, age:boyos, phone} =actor
-console.log(name)
-console.log(boyos)
-console.log(phone)
